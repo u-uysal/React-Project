7505ed98e8cfb2e2919106e4982eabac0dc29068
ufuk <ufuk.uysl5@gmail.com>
2020-02-03 22:36:24 +0100

    Camelcase style

---
+++ React Styling/camelCase.js
@@ -0,0 +1,10 @@
+class MyHeader extends React.Component {
+    render() {
+      return (
+        <div>
+        <h1 style={{backgroundColor: "lightblue"}}>Hello Style!</h1>
+        <p>Add a little style!</p>
+        </div>
+      );
+    }
+  }
