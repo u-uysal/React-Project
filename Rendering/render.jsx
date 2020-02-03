0b357423ff3ede561d6d9332f01a845ca0b1eb9d
ufuk <ufuk.uysl5@gmail.com>
2020-02-03 22:28:29 +0100

    render statement

---
+++ Rendering/render.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+//ReactDOM.render(WHAT DO I WANT TO RENDER, WHERE DO I WANT TO RENDER IT)
+ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"))
+// Second important things you can use only one tag side by side in render attirubute.
