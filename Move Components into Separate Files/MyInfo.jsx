b5fb72bf59eb575433d7737763cac102bee67854
ufuk <ufuk.uysl5@gmail.com>
2020-02-03 22:30:15 +0100

    Move components separate file

---
+++ Move Components into Separate Files/MyInfo.jsx
@@ -0,0 +1,17 @@
+import React from "react" // dont forget import this one.
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Bob Ziroll</h1>
+      <p>This is a paragraph about me...</p>
+      <ul>
+        <li>Thailand</li>
+        <li>Japan</li>
+        <li>Nordic Countries</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MyInfo // you need to export to use in other file
---
+++ Move Components into Separate Files/components/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import MyInfo from "./components/MyInfo" // importing the exporting components
+
+ReactDOM.render(<MyInfo />, document.getElementById("root"))
