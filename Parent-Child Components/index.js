412fe9e7b578d6b4f207b5a61d8162b4024f7952
ufuk <ufuk.uysl5@gmail.com>
2020-02-03 22:31:16 +0100

    Creating parent/child components

---
+++ Parent-Child Components/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "./App" //Attention : We used only App component, not others.Because App is parent
+
+ReactDOM.render(<App />, document.getElementById("root"))
