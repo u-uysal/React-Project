d470e6665e0dce96671a6fba6f5137a69d15f8da
ufuk <ufuk.uysl5@gmail.com>
2020-02-04 21:38:12 +0100

    Using High Ordering Functions in React

---
+++ Mapping Components in React/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+import Joke from "./Joke"
+import jokesData from "./jokesData"
+
+
+
+function App() {
+    const jokeComponents = jokesData.map(joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />)
+    
+    return (
+        <div>
+            {jokeComponents}            
+        </div>
+    )
+}
+
+export default App
