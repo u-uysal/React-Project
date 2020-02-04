fd043afbc7217c26916bd176715055ab50a062bc
ufuk <ufuk.uysl5@gmail.com>
2020-02-04 22:25:32 +0100

    Some important comments

--- Mapping Components in React/App.jsx
+++ Mapping Components in React/App.jsx
@@ -7,10 +7,11 @@ import jokesData from "./jokesData"
 
 function App() {
     const jokeComponents = jokesData.map(joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />)
-    
+    // When you use the map function, it returns always new array with looping
+    // İf you are using map function dont forget to use key attribute. And key must be unique.
     return (
         <div>
-            {jokeComponents}            
+            {{jokeComponents}  /* new array of JokeData with props   */}
         </div>
     )
 }
