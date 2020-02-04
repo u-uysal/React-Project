779c3db926befae55d280c004318d417330814cd
ufuk <ufuk.uysl5@gmail.com>
2020-02-04 23:25:21 +0100

    Creating App.js file

---
+++ Mapping Components Practise/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Product from "./Product"
+import productsData from "./vschoolProducts"
+
+function App() {
+    const productComponents = productsData.map(item => <Product key={item.id} product={item}/>)
+    
+    return (
+        <div>
+            {productComponents}
+        </div>
+    )
+}
+
+export default App
