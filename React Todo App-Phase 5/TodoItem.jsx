1eefb72173831c8bda8a32fb6af06403f997c958
ufuk <ufuk.uysl5@gmail.com>
2020-02-05 19:47:35 +0100

    Updating Todo Project with using Event Handler

---
+++ React Todo App-Phase 5/TodoItem.jsx
@@ -0,0 +1,20 @@
+/**
+ * Challenge: Get rid of our warning about not having an onChange on our input. For now, the function that runs onChange can simply console.log something.
+ */
+
+import React from "react"
+
+function TodoItem(props) {
+    return (
+        <div className="todo-item">
+            <input 
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={() => console.log("Changed!")}
+            />
+            <p>{props.item.text}</p>
+        </div>
+    )
+}
+
+export default TodoItem
