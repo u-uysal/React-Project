a6378edb28f19503bde0fda47318558558022fee
ufuk <ufuk.uysl5@gmail.com>
2020-02-07 18:38:11 +0100

    Correction Todo completed issue

--- React Todo App-Phase 6/App.js
+++ React Todo App-Phase 6/App.js
@@ -24,9 +24,22 @@ class App extends React.Component {
     }
     
     handleChange(id) {
-        // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
-        // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
-        console.log("Changed", id)
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
+                }
+                return todo
+            })
+            console.log(prevState.todos)
+            console.log(updatedTodos)
+            return {
+                todos: updatedTodos
+            }
+        })
     }
     
     render() {
