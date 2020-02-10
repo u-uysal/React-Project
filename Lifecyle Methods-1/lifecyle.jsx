d5b7758c0d0b24154f542dbcbd4b77779c53a73b
ufuk <ufuk.uysl5@gmail.com>
2020-02-10 14:52:20 +0100

    ComponentDidMount

---
+++ Lifecyle Methods-1/lifecyle.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from "react"
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+    
+    componentDidMount() { // Means that component just born. It runs only once
+        
+    }
+    
+    render() {
+        return (
+            <div>
+                Code goes here
+            </div>
+        )
+    }
+}
+
+export default App
