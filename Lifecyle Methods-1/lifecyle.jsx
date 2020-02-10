c889440fd7def4b613166537d13689d654308ea9
ufuk <ufuk.uysl5@gmail.com>
2020-02-10 14:59:53 +0100

    componentWillUnmount

--- Lifecyle Methods-1/lifecyle.jsx
+++ Lifecyle Methods-1/lifecyle.jsx
@@ -18,6 +18,10 @@ class App extends Component {
         // return true if want it to update
         // return false if not
     }
+    componentWillUnmount() {
+        // teardown or cleanup your code before your component disappears
+        // (E.g. remove event listeners)
+    }
     
     render() {
         return (
