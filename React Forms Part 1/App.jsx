485b6f423bbbf9f38ed15df38dff61a00901b5de
ufuk <ufuk.uysl5@gmail.com>
2020-02-12 16:38:31 +0100

    comments

--- React Forms Part 1/App.jsx
+++ React Forms Part 1/App.jsx
@@ -13,7 +13,7 @@ class App extends Component {
     handleChange(event) {
         this.setState({
             [event.target.name]: event.target.value // kutulu paranteze almasının nedeni event objesinde ulaşmak
-                                                    //istediğimiz değerin string olmasıdır
+                                                    //istediğimiz değerin string olması
         })
     }
     
