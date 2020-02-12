299abd66764e9d1ba45a04c1bc539c8d2359e1f9
ufuk <ufuk.uysl5@gmail.com>
2020-02-12 22:25:22 +0100

    Using API for memeGenerator

--- React Meme Generator Capstone Project/MemeGenerator.js
+++ React Meme Generator Capstone Project/MemeGenerator.js
@@ -1,21 +1,32 @@
 import React, {Component} from "react"
 
-/**
- * Initialize state to save the following data:
- *      top text
- *      bottom text
- *      random image (intialize with "http://i.imgflip.com/1bij.jpg")
- */
-
 class MemeGenerator extends Component {
     constructor() {
         super()
         this.state = {
             topText: "",
             bottomText: "",
-            randomImg: "http://i.imgflip.com/1bij.jpg"
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
         }
     }
+    
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => {
+                const {memes} = response.data
+                this.setState({ allMemeImgs: memes })// We dont care about previos state. We can write like this.
+            })
+    }
+    /**
+     * We'll be using an API that provides a bunch of meme images.
+     * 
+     * Your task:
+     * make an API call to "https://api.imgflip.com/get_memes" and save the 
+     * data that comes back (`response.data.memes`) to a new state property
+     * called `allMemeImgs`. (The data that comes back is an array)
+     */
     
     render() {
         return (
