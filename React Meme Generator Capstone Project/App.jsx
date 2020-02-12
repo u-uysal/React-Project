70c7473be081fcf28876aa4665d771fedafc8291
ufuk <ufuk.uysl5@gmail.com>
2020-02-12 22:14:50 +0100

    İmporting MemeGenarator and Header Component

--- React Meme Generator Capstone Project/App.jsx
+++ React Meme Generator Capstone Project/App.jsx
@@ -1,8 +1,13 @@
 import React from "react"
+import Header from "./Header"
+import MemeGenerator from "./MemeGenerator"
 
 function App() {
     return (
-        <h1>Hello world!</h1>
+        <div>
+            <Header />
+            <MemeGenerator />
+        </div>
     )
 }
 
