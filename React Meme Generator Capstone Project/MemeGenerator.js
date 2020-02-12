9e7fe8a0db4fc8c74a03ec2d4d7347d263c1b150
ufuk <ufuk.uysl5@gmail.com>
2020-02-12 22:19:38 +0100

    Adding state

--- React Meme Generator Capstone Project/MemeGenerator.js
+++ React Meme Generator Capstone Project/MemeGenerator.js
@@ -1,14 +1,25 @@
 import React, {Component} from "react"
 
+/**
+ * Initialize state to save the following data:
+ *      top text
+ *      bottom text
+ *      random image (intialize with "http://i.imgflip.com/1bij.jpg")
+ */
+
 class MemeGenerator extends Component {
     constructor() {
         super()
-        this.state ={}
+        this.state = {
+            topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg"
+        }
     }
     
     render() {
         return (
-            
+            <h1>MEME GENERATOR SECTION</h1>
         )
     }
 }
