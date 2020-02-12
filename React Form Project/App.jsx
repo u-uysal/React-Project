fc19c1f5eccf98aad710754c1d9dcd84ea5d0bdd
ufuk <ufuk.uysl5@gmail.com>
2020-02-12 17:03:24 +0100

    Adding Select and Options elements

--- React Form Project/App.jsx
+++ React Form Project/App.jsx
@@ -69,7 +69,7 @@ class App extends Component {
                             type="radio" 
                             name="gender"
                             value="male"
-                            checked={this.state.gender === "Male"}
+                            checked={this.state.gender === "male"}
                             onChange={this.handleChange}
                         /> Male
                     </label>
@@ -81,14 +81,25 @@ class App extends Component {
                             type="radio" 
                             name="gender"
                             value="female"
-                            checked={this.state.gender === "Female"}
+                            checked={this.state.gender === "female"}
                             onChange={this.handleChange}
                         /> Female
                     </label>
                     
                     <br />
                     
-                    {/* Create select box for location here */}
+                    <select  // select and options elements are added
+                        value={this.state.destination} 
+                        name="destination" 
+                        onChange={this.handleChange}
+                    >
+                        <option value="">-- Please Choose a destination --</option>
+                        <option value="germany">Germany</option>
+                        <option value="norway">Norway</option>
+                        <option value="north pole">North Pole</option>
+                        <option value="south pole">South Pole</option>
+                    </select>
+                    
                     <br />
                     
                     {/* Create check boxes for dietary restrictions here */}
@@ -101,7 +112,7 @@ class App extends Component {
                 <p>Your name: {this.state.firstName} {this.state.lastName}</p>
                 <p>Your age: {this.state.age}</p>
                 <p>Your gender: {this.state.gender}</p>
-                <p>Your destination: {/* Destination here */}</p>
+                <p>Your destination: {this.state.destination}</p>
                 <p>
                     Your dietary restrictions: 
                     {/* Dietary restrictions here, comma separated */}
