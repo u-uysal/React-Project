29d581ce90b8208c850e9c080b8eec2396a721e4
ufuk <ufuk.uysl5@gmail.com>
2020-02-13 16:59:07 +0100

    Adding extra Decrement function

--- Hooks/App.js
+++ Hooks/App.js
@@ -7,13 +7,17 @@ function App() {
     const [count, setCount] = useState(0)
     
     function increment() {
-        setCount(prevCount => prevCount + 1) // Creating increment function for changing value of count
+        setCount(prevCount => prevCount + 1)
+    }
+    function decrement() {
+        setCount(prevCount => prevCount - 1)
     }
     
     return (
         <div>
             <h1>{count}</h1>
             <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
         </div>
     )
 }
