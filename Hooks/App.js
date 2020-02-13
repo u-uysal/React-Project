0a009e52d1961ff2e2570e37126f6cfd858f0728
ufuk <ufuk.uysl5@gmail.com>
2020-02-13 17:30:39 +0100

    Using setInterval function with useEffect

--- Hooks/App.js
+++ Hooks/App.js
@@ -1,27 +1,23 @@
 import React, {useState, useEffect} from "react"
-import randomcolor from "randomcolor" // we imported that.
+import randomcolor from "randomcolor"
 
 function App() {
     const [count, setCount] = useState(0)
-    const [color, setColor] = useState("") // initial value is empty string.
+    const [color, setColor] = useState("")
     
-    function increment() {
-        setCount(prevCount => prevCount + 1)
-    }
-    
-    function decrement() {
-        setCount(prevCount => prevCount - 1)
-    }
+    useEffect(() => { // we used second useEffect because we want to change automaticly value of count
+        setInterval(() => {
+            setCount(prevCount => prevCount + 1)
+        }, 1000)
+    }, [])
     
     useEffect(() => {
-        setColor(randomcolor())// whenever count changed,color will change 
+        setColor(randomcolor())
     }, [count])
     
     return (
         <div>
             <h1 style={{color: color}}>{count}</h1>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
         </div>
     )
 }
