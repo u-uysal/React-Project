1ed5537157e45167066510f0d2c3cd56e9a49dd2
ufuk <ufuk.uysl5@gmail.com>
2020-02-13 16:18:34 +0100

    Array destructuring

--- Hooks/App.js
+++ Hooks/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react"
 
 function App() {
-    const value = useState("Yes")
+    const [ value ] = useState("No") // array destructuring
     console.log(value)
+    
     
     return (
         <div>
-          <h1>Is state important to know? {value[0]}</h1>   {/* value[0] = Yes */}
+        <h1>Is state important to know? {value}</h1>      {/* We dont need to write index number anymore.Because 
+                                                               we use array destructuring*/}
         </div>
     )
 }
