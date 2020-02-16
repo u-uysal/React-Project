d310f40c987b15ce255f4d9c96e8708ade2077b6
ufuk <ufuk.uysl5@gmail.com>
2020-02-16 19:31:27 +0100

    Passing Argument in EventHandling

--- React Recap/App.jsx
+++ React Recap/App.jsx
@@ -8,15 +8,15 @@ export default class App extends Component {
         if(this.state.tags.length === 0 ) return "There is no tags."
         return  <ul>{this.state.tags.map((tag)=><li key = {tag}>{tag}</li>)}</ul>
     }
-    eventHandlig(){
+    eventHandlig =()=>{ // Eğer arrow fonksiyonu kullanıyorsan "Bind" methoduna gerek yok
         console.log("Event Handling")
     }
     render() {
         return (
             <div>
                 {this.state.renderTags()}
-                <button onClick= {this.eventHandlig}></button>
-                                {/* Note that there is no parantheses cause we dont need to call function */}
+                <button onClick={() => this.handleClick(id)} />
+                {/* Passing Argument. we can use arrow function */}
             </div>
         )
     }
