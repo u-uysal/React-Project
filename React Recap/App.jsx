f88a8166acf5aeac897f419c03b9ea542cb1f942
ufuk <ufuk.uysl5@gmail.com>
2020-02-16 18:47:47 +0100

    Event Handling

--- React Recap/App.jsx
+++ React Recap/App.jsx
@@ -8,10 +8,15 @@ export default class App extends Component {
         if(this.state.tags.length === 0 ) return "There is no tags."
         return  <ul>{this.state.tags.map((tag)=><li key = {tag}>{tag}</li>)}</ul>
     }
+    eventHandlig(){
+        console.log("Event Handling")
+    }
     render() {
         return (
             <div>
-                {this.state.renderTags()}                
+                {this.state.renderTags()}
+                <button onClick= {this.eventHandlig}></button>
+                                {/* Note that there is no parantheses cause we dont need to call function */}
             </div>
         )
     }
