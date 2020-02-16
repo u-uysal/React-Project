f316d9806a9b50112fb230816916e1fe94629ecf
ufuk <ufuk.uysl5@gmail.com>
2020-02-16 18:37:46 +0100

    Conditional Rendering 1

--- React Recap/App.jsx
+++ React Recap/App.jsx
@@ -4,10 +4,14 @@ export default class App extends Component {
     state = {
         tags = ["a1","b2","c3"]
     };
+    renderTags () {
+        if(this.state.tags.length === 0 ) return "There is no tags."
+        return  <ul>{this.state.tags.map((tag)=><li key = {tag}>{tag}</li>)}</ul>
+    }
     render() {
         return (
             <div>
-                <ul>{this.state.tags.map((tag)=><li key = {tag}>{tag}</li>)}</ul>
+                {this.state.renderTags()}                
             </div>
         )
     }
