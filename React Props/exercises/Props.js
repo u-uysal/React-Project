6731c1fef98680a288152edeec83041f4fc8e8fd
ufuk <ufuk.uysl5@gmail.com>
2020-02-18 22:07:30 +0100

    Added extra props exercises

---
+++ React Props/exercises/Props.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Exercises from '../Exercises';
+
+class Apps extends Component {
+    render() {
+        return (
+            <div>
+                <Exercises name ="Ufuk"/>
+                <Exercises name ="UYSAL"/>
+            </div>
+        )
+    }
+}
